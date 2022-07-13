a95757d8373dbd279aa1ed29b3018814a4c1f7a9
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-13 17:48:52 +0500

    MERN_BLOG

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -26,7 +26,7 @@ router.put("/:id", async (req, res) => {
           },
           { new: true }
         );
-        res.status(200).json(updatePost)
+        res.status(200).json(updatePost);
       } catch (err) {
         // res.status;
       }
@@ -44,8 +44,8 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
-        await post.delete()
-        res.status(200).json("Post has been deleted...")
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
       } catch (err) {
         // res.status;
       }
@@ -57,13 +57,32 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-
 // GET POST
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts
+    if(username) {
+      posts = await Post.find({username})
+    } else if(catName) {
+      posts = await Post.find({categories: {
+        $in: [catName]
+      }})
+    } else {
+      posts = await Post.find()
+    }
+    res.status(200).json(posts)
   } catch (err) {
     res.status(500).json(err);
   }
