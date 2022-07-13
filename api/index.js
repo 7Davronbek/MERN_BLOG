ffef8cfa13acda1c3eca7c223ce17bd21ad640d9
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-13 16:25:33 +0500

    MERN_BLOG

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
 
 dotenv.config();
 app.use(express.json())
@@ -17,6 +18,7 @@ mongoose
 
   app.use('/api/auth', authRoute)
   app.use('/api/users', userRoute)
+  app.use('/api/posts', postRoute)
 
 app.listen("5000", () => {
   console.log("Backend is running...");
