6841a3ce8f30cf75b3f809b4bad20597552b6e48
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-14 11:20:02 +0500

    MERN_BLOG

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -72,17 +72,19 @@ router.get("/", async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
   try {
-    let posts
-    if(username) {
-      posts = await Post.find({username})
-    } else if(catName) {
-      posts = await Post.find({categories: {
-        $in: [catName]
-      }})
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
     } else {
-      posts = await Post.find()
+      posts = await Post.find();
     }
-    res.status(200).json(posts)
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
