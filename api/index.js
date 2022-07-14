f8971207dc173204555a6e6984a943db77c2618b
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-14 16:14:08 +0500

    MERN_BLOG

--- api/index.js
+++ api/index.js
@@ -7,7 +7,9 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const cors = require('cors')
 
+app.use(cors());
 dotenv.config();
 app.use(express.json());
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+
+    const fetchPost = async () => {
+      const res = await axios.get('/posts')
+      console.log(res);
+    }
+
+    
+    fetchPost()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>App</>
+  )
 }
 
-export default App;
+export default App
