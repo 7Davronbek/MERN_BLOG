ff2593e682424d0964c850109a4aba7683d19b93
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-16 11:05:26 +0500

    MERN_BLOG

--- client/src/componets/Navbar.jsx
+++ client/src/componets/Navbar.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import InputMask from "react-input-mask";
 
 const Navbar = () => {
-  return (
-    <div className='Navbar'>
-        <div className="container">
-            <div className="row">
-                <div className="col-2">
-                    <h1>Times news</h1>
-                </div>
-                <div className="col-6">
-                    <div className="ulWrap">
-                        <a href="/">Home</a>
-                        <a href="/">Blogs</a>
+    return (
+        <div className='Navbar'>
+            <div className="container">
+                <div className="row">
+
+                    <InputMask
+                        // value={phone}
+                        // onChange={e => setPhone(e.target.value)}
+                        mask="(99) 999-99-99"
+                        maskChar=" "
+                    />
+                    <div className="col-2">
+                        <h1>Times news</h1>
+                    </div>
+                    <div className="col-6">
+                        <div className="ulWrap">
+                            <a href="/">Home</a>
+                            <a href="/">Blogs</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
