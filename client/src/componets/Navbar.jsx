14cf8f3189df787dda64f1de9238558256a4fe48
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-18 16:14:29 +0500

    MERN_BLOG

--- client/src/componets/Navbar.jsx
+++ client/src/componets/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='Navbar'>
             <div className="container">
-                <div className="row">
+                <div className="row align-items-center">
 
-                    <div className="col-2">
-                        <h1>Blog's</h1>
+                    <div className="col-3">
+                        <h1><Link to='/'>Blog's</Link></h1>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 ms-auto">
                         <div className="ulWrap">
                             <a href="/">Home</a>
                             <a href="/">Blogs</a>
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/main.scss";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
