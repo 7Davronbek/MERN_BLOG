fe9549b2e6b7223bb1d485c29ac38fc88ee2c481
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-21 13:29:37 +0500

    MERN_BLOG

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+    }
+    
     return (
-        <div className='Login'>
+        <div className='Login Register'>
             <div className="container">
                 <div className="row">
                     <div className="col-12">
                         <h1 className="h1 text-center">Login</h1>
+
+                        <form onSubmit={handleLogin}>
+                            {error && <h6>Something went wrong! Try again!</h6>}
+                            
+                            {/* <label htmlFor="Username">Username</label>
+                            <input
+                                type="text"
+                                id='Username'
+                                className="form-control"
+                                onChange={e => setUsername(e.target.value)}
+                            /> */}
+
+                            <label htmlFor="Email">Email</label>
+                            <input
+                                type="email"
+                                id='Email'
+                                className="form-control"
+                                onChange={e => setEmail(e.target.value)}
+                            />
+
+                            <label htmlFor="Password">Password</label>
+                            <input
+                                type="password"
+                                id='Password'
+                                className="form-control"
+                                onChange={e => setPassword(e.target.value)}
+                            />
+
+                            <button type="submit" className='btn'>Login</button>
+                            <Link to='/register'>Don't have an account?</Link>
+                        </form>
+                        
                     </div>
                 </div>
             </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,71 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Register = () => {
-  return (
-    <div className='Register'>
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h1 className="h1 text-center">Register</h1>
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+
+    const navigate = useNavigate()
+
+    const handleRegister = async (e) => {
+        setError(false)
+        e.preventDefault()
+
+        try {
+            const res = await axios.post('/auth/register', { username, email, password })
+            res.data && navigate("/login", { replace: true })
+            
+        } catch (err) {
+            setError(true)
+        }
+
+    }
+
+    return (
+        <div className='Register'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="h1 text-center" >Register</h1>
+
+                        <form onSubmit={handleRegister}>
+                            {error && <h6>Something went wrong! Try again!</h6>}
+                            <label htmlFor="Username">Username</label>
+                            <input
+                                type="text"
+                                id='Username'
+                                className="form-control"
+                                onChange={e => setUsername(e.target.value)}
+                            />
+
+                            <label htmlFor="Email">Email</label>
+                            <input
+                                type="email"
+                                id='Email'
+                                className="form-control"
+                                onChange={e => setEmail(e.target.value)}
+                            />
+
+                            <label htmlFor="Password">Password</label>
+                            <input
+                                type="password"
+                                id='Password'
+                                className="form-control"
+                                onChange={e => setPassword(e.target.value)}
+                            />
+
+                            <button type="submit" className='btn'>Register</button>
+                            <Link to='/login'>Already have an account?</Link>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Register
