57c2c232ca187e67c124d2572c46b0f2cfc9d7ac
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-25 17:28:17 +0500

    MERN_BLOG

--- client/src/pages/PostId.jsx
+++ client/src/pages/PostId.jsx
@@ -1,16 +1,16 @@
 import axios from 'axios'
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { connect, useDispatch } from 'react-redux'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { PF } from '../tools/contstants'
 
-import { handleDelete } from '../redux/actions/postAction'
+import { postupdateState, handleDelete } from '../redux/actions/postAction'
 
 const PostId = (props) => {
     const [post, setPost] = useState({})
     const location = useLocation()
+    const dispatch = useDispatch()
     const navigate = useNavigate()
     const path = location.pathname.split('/')[2]
 
@@ -30,27 +30,38 @@ const PostId = (props) => {
 
                     <div className="col-9">
 
-                        {/* {post.photo && */}
+                        {/* { */}
                         <div className="img">
                             <img src={PF + post.photo} alt="" />
                         </div>
                         {/* } */}
 
-                        <div className="top d-flex align-items-center   ">
-                            <h1>{post.title}</h1>
-                            {post.username === props.user?.username && (
-                                <div className="d-flex aling-items-center ms-auto">
-                                    <button className="btn btn-outline-dark me-3">Edit</button>
-                                    <button onClick={(id, user, nav) => props.handleDelete(post._id, props.user.username, navigate)} className="btn btn-outline-danger">Delete</button>
-                                </div>
-                            )
-                            }
+                        <div className="top d-flex align-items-center">
+                            {props.updateMode ? (<>
+                                <input value={post.title} autoFocus type="text" className='form-control h2' />
+                            </>) : (<>
+                                <h1>{post.title}</h1>
+                                {post.username === props.user?.username && (
+                                    <div className="d-flex aling-items-center ms-auto">
+                                        <button onClick={() => dispatch(postupdateState({ updateMode: true }))} className="btn btn-outline-dark me-3">Edit</button>
+                                        <button onClick={(id, user, nav) => props.handleDelete(post._id, props.user.username, navigate)} className="btn btn-outline-danger">Delete</button>
+                                    </div>
+                                )}
+                            </>)}
                         </div>
                         <div className="author d-flex align-items-center justify-content-between my-2">
                             <h5>Author: <Link to={`/?user=${post.username}`}>{post.username}</Link></h5>
                             <i>{new Date(post.createdAt).toDateString()}</i>
                         </div>
-                        <h6>{post.desc}</h6>
+                        {props.updateMode ? (<>
+                            <textarea value={post.desc} className='form-control h6' cols="10" rows="10"></textarea>
+                        </>) : (<>
+                            <h6>{post.desc}</h6>
+                        </>)}
+
+                        {props.updateMode && <>
+                            <button onClick={() => dispatch(postupdateState({ updateMode: false }))} className="btn myBtn">Update Post</button>
+                        </>}
                     </div>
 
 
@@ -63,8 +74,8 @@ const PostId = (props) => {
 const mapStateToProps = state => {
     return {
         user: state.auth.user,
-        updateMode: state.post
+        updateMode: state.post.updateMode
     }
 }
 
-export default connect(mapStateToProps, { handleDelete })(PostId)
+export default connect(mapStateToProps, { postupdateState, handleDelete })(PostId)
